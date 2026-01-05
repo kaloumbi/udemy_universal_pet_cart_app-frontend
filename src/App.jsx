d8e299bce6d5f567efe576419990da6993525cef
Paterne <diedhioupaterne279@gmail.com>
2026-01-05 12:25:49 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/home/Home";
+
+function App() {
+  return (
+    <main className="">
+      <div className="text-info">Welcome to the brand new project</div>
+      <Home />
+    </main>
+  );
+}
+
+export default App;
