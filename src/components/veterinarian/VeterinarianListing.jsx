fd3ab1a366fecbe0448f209d8c626b26f6bec183
Paterne <diedhioupaterne279@gmail.com>
2026-01-26 14:50:52 +0000

    displaying card

--- src/components/veterinarian/VeterinarianListing.jsx
+++ src/components/veterinarian/VeterinarianListing.jsx
@@ -5,39 +5,34 @@ import { getVeterinarians } from "./VeterinarianService";
 
 const VeterinarianListing = () => {
   const [veterinarians, setVeterinarians] = useState([]);
-
   const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     getVeterinarians()
       .then((data) => {
-        console.log("Données reçues de l'API :", data.data); // <--- AJOUTE CECI
+        console.log("Données reçues de l'API :", data.data);
         setVeterinarians(data.data);
       })
       .catch((error) => {
         setErrorMessage(error.message);
-      }, []);
-  });
+      });
+  }, []); // <-- La dépendance est maintenant placée ici
 
   return (
-    <Row>
-      <h6>Meet our Veterinarians</h6>
-      {errorMessage && <div style={{color : 'red'}} > {errorMessage} </div>}
-      <Col>
-        <Container>
-          <Row>The search goes here</Row>
-        </Container>
-      </Col>
-      <Col>
-        <Container>
-          <Row>
-            {veterinarians.map((vet, index) => {
-              <VeterinarianCard key={index} vet={vet} />;
-            })}
-          </Row>
-        </Container>
-      </Col>
-    </Row>
+    <Container>
+      <Row className="justify-content-center">
+        <h6 className="text-center mb-4">Meet our Veterinarians</h6>
+      </Row>
+      <Row className="justify-content-center">
+        {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+        <Col md={4}>The search goes here</Col>
+        <Col md={7}>
+          {veterinarians.map((vet, index) => (
+            <VeterinarianCard key={index} vet={vet} />
+          ))}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
